f2558ec0d3fd0adac86b0b4ad7ad0d6fcca1d05f
Tyzaya August <2023159774@ub.edu.bz>
2025-10-07 15:07:37 +0000

    Added the insert iterative.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ struct BSTNode {
 };
 
 void insertRecursive(BSTNode*& root, int value);
+void insertIterative(BSTNode*& root, int value);
 
 
 int main() {
@@ -23,13 +24,39 @@ int main() {
 }
 
 void insertRecursive(BSTNode*& root, int value) {
-if(!root) {
+  if(!root) {
      root = new BSTNode(value);
      return;
 }
-if(value < root->data) {
+   if(value < root->data) {
     insertRecursive(root->left, value);
 } else if(value > root->data) {
     insertRecursive(root->right, value);
 }
 }
+
+void insertIterative(BSTNode*& root, int value) {
+    if (!root) {
+     root = new BSTNode(value);
+     return;
+}
+// Set up a trailing pointer
+BSTNode* cur = root;
+BSTNode* parent = nullptr;
+while (cur) {
+    parent = cur;
+    if(value < cur->data) {
+        cur = cur->left;
+    } else if(value > cur->data) {
+         cur = cur->right;
+    } else {
+         return; // duplicate so ignore
+     }
+}
+if(value < parent->data) {
+    parent->left = new BSTNode(value);
+} else {
+    parent->right = new BSTNode(value);
+}
+}
+
